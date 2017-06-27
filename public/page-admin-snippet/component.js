10b5292177306d78ac9d09a648304c483b110b3b
Alexey Berdnikov <aberdnikov@gmail.com>
2017-06-27 16:04:36 +0500

    fixed

--- public/page-admin-snippet/component.js
+++ public/page-admin-snippet/component.js
@@ -19,7 +19,44 @@
 
         //получаем настройки списка
         $ctrl.snippets = {};
+        $ctrl.params = {};
 
+        $ctrl.isShowContext = function (context) {
+            if (undefined == $ctrl.params.context) {
+                return true;
+            }
+            if ($ctrl.params.context.length == 0) {
+                return true;
+            }
+            return (context.toLowerCase()).indexOf($ctrl.params.context.toLowerCase()) !== -1;
+        };
+        $ctrl.isShowCode = function (code, snippet) {
+            var cnt = 0;
+            _.each(snippet.langs, function(val, lang){
+                if($ctrl.isShowTranslate(val)){
+                    cnt++;
+                }
+            });
+            if(!cnt){
+                return false;
+            }
+            if (undefined == $ctrl.params.code) {
+                return true;
+            }
+            if ($ctrl.params.code.length == 0) {
+                return true;
+            }
+            return cnt && (code.toLowerCase()).indexOf($ctrl.params.code.toLowerCase()) !== -1;
+        };
+        $ctrl.isShowTranslate = function (translate) {
+            if (undefined == $ctrl.params.translate) {
+                return true;
+            }
+            if ($ctrl.params.translate.length == 0) {
+                return true;
+            }
+            return (translate.toLowerCase()).indexOf($ctrl.params.translate.toLowerCase()) !== -1;
+        };
         //функция загрузки данных
         $ctrl.load = function () {
             $http
